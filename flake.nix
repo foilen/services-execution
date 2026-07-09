9df7a8d8a726d18b486f87e92d13606060cd8355
Simon Levesque <simon@foilen.com>
2026-07-08 21:42:37 -0400

    Release 1.2.0

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         packages.default = pkgs.buildGoModule {
           pname = "services-execution";
-          version = "1.1.0";
+          version = "1.2.0";
 
           src = ./.;
 
